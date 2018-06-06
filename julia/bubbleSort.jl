1972bcbf39c236e7ded9f4651042267e6c38b220
szmlb <szmlb.robotics@gmail.com>
2018-06-06 20:43:01 +0900

    First commit: add julia/bubbleSort.jl

---
+++ julia/bubbleSort.jl
@@ -0,0 +1,41 @@
+function bubbleSort(sort_list)
+
+    flag = 1
+
+    while flag == 1
+      flag = 0
+      for i in 1:length(sort_list)-1
+        if sort_list[i] > sort_list[i+1]
+          # 左右の並びがおかしければ入れ替える
+          flag = 1
+          tmp = sort_list[i]
+          sort_list[i] = sort_list[i+1]
+          sort_list[i+1] = tmp
+        end
+        # 一度も並べ替えをせずに見終わったら終了
+      end
+    end
+
+end
+
+function main()
+
+  # list to be sorted
+  rng = MersenneTwister(1234);
+  list_size = 100
+  sort_list = shuffle(rng, Vector(1:list_size))
+
+  print("List to be sorted:")
+  println(sort_list)
+
+  # Sort
+  @time bubbleSort(sort_list)
+
+  print("Sorted list:")
+  println(sort_list)
+
+end
+
+if contains(@__FILE__, PROGRAM_FILE)
+    main()
+end
