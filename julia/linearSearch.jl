3e99704463bc3fcbe169f8ba3adabc2f0d8b051b
szmlb <szmlb.robotics@gmail.com>
2018-08-23 20:36:31 +0900

    Add linearSearchWithSentinel() function

--- julia/linearSearch.jl
+++ julia/linearSearch.jl
@@ -14,6 +14,31 @@ function linearSearch(search_list, search_num)
 
 end
 
+function linearSearchWithSentinel(search_list, search_num)
+
+  # 最後の値をsearch_listに入れる(番兵)
+  tmp = Int(search_list[length(search_list)])
+  search_list[length(search_list)] = Int(search_num)
+
+  num = 1
+  while true
+    if search_num == search_list[num]
+      break
+    end
+    num += 1
+  end
+
+  search_list[length(search_list)] = tmp # 配列最後の値を元に戻す
+  if num < length(search_list)
+     return num # 最後以外で一致
+  elseif search_num == tmp
+     return num # 最後で一致
+  else
+     return false
+  end
+
+end
+
 function main()
 
   # list to be sorted
@@ -24,21 +49,23 @@ function main()
 
   # Search
   search_num = 1000
-  index_found = @time linearSearch(search_list, search_num)
+  index_found1 = @time linearSearch(search_list, search_num)
+  index_found2 = @time linearSearchWithSentinel(search_list, search_num)
 
   print("Search for : ")
   println(search_num)
 
-  if index_found == false
+  if index_found1 == false || index_found2 == false
     println("Nothing found.")
     return
   end
 
   print("Found index: ")
-  println(index_found)
+  print(index_found1); print(" ")
+  println(index_found2)
 
   print("Found value is ... ")
-  println(Int(search_list[index_found]))
+  println(Int(search_list[index_found1]))
 
 end
 
