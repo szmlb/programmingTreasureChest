519cf70d1143357a70202e014a88dd07077eed12
szmlb <szmlb.robotics@gmail.com>
2018-08-26 13:24:14 +0900

    Modified binarySearch for duplicated values

--- julia/binarySearch.jl
+++ julia/binarySearch.jl
@@ -19,17 +19,39 @@ function binarySearch(search_list, search_num)
 
 end
 
+function binarySearchDuplicated(search_list, search_num)
+
+  left = 1
+  right = length(search_list)
+  while left <= right
+    mid = round(Int, (left + right) / 2, RoundNearestTiesUp)
+    if search_list[mid] < search_num
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+  if search_num == search_list[left]
+    return left
+  end
+
+  return false
+
+end
+
 function main()
 
   # list to be sorted
   rng = MersenneTwister(1234);
   list_size = 1e7
   search_list = Vector(1:list_size) # The list is to be sorted for binary search
-  #search_list = shuffle(rng, Vector(1:list_size))
+  search_list[298] = 300
+  search_list[299] = 300
 
   # Search
   search_num = 300
-  index_found = @time binarySearch(search_list, search_num)
+  #index_found = @time binarySearch(search_list, search_num)
+  index_found = @time binarySearchDuplicated(search_list, search_num)
 
   print("Search for : ")
   println(search_num)
