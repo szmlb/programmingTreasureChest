5daa01c4a422ee11f90d0107278a073323a0d409
szmlb <szmlb.robotics@gmail.com>
2018-09-13 00:40:31 +0900

    Modify hashMap.jl

--- julia/hashMap.jl
+++ julia/hashMap.jl
@@ -75,7 +75,7 @@ function addDataToMap(hashtable, newdata)
 end
 
 # 英単語keyに対応する和訳をhashtableから探し出して返す
-function GetDataFromMap(hashtable, key)
+function getDataFromMap(hashtable, key)
 
     # 探索を開始するハッシュ値を求める
     hashval = MakeHash2(key, hashtable.size)
@@ -95,7 +95,122 @@ function GetDataFromMap(hashtable, key)
     return nothing
 end
 
+# 英単語keyに関するデータをhashtableから取り除く(データそのものは削除しない)
+function deleteDataFromMap(hashtable, key)
+
+    # 探索を開始するハッシュ値を求める
+    hashval = MakeHash2(key, hashtable->size)
+
+    # その位置から順番に，keyと同じ値をもつデータが現れるまで探索を行う
+    for k in 0:Int(hashtable->size/2)
+        word = hashtable->data[(hashval + k * k) % hashtable->size]
+        if word != nothing
+            if cmp(key, word.english) == 0
+                hashtable->data[(hashval + k * k) % hashtable.size] = nothing
+                # ハッシュテーブルから取り除いたデータを返す
+                return word
+            end
+        end
+    end
+
+    # ハッシュ表サイズの半分に相当する回数探し続けても
+    # 見つからない場合は，該当するデータがハッシュ表のなかに
+    # ないことを意味する
+    return nothing
+end
+
+# ハッシュテーブルを指定したサイズに初期化する
+function initHashTable(hashtable, size)
+    hashtable.data = Array{wordSet}(undef, size)
+    hashtable.size = size
+
+end
+
+#=
+# ハッシュテーブルのクリーンアップ
+function cleanupHashTable(hashtable)
+    free(hashtable.data)
+    hashtable->size = 0
+end
+=#
+
+# hashtable中の全データを表示する（単なる表示用）
+function printAllData(hashtable)
+    for n in 1:hashtable.size
+        if hashtable.data[n] != nothing
+            @printf "%d:\t%s\t%s\n" n hashtable.data[n].english hashtable.data[n].japanese
+        end
+    end
+end
+
 function main()
+
+mutable struct wordSet
+    english::String
+    japanese::String
+    wordSet() = new()
+end
+
+    hash_table = hashTable()
+    word_found = wordSet()
+
+    #words = Array{wordSet}(undef,n)
+    #words = Array{wordSet,1}(undef,n)
+    words = Vector{wordSet}(5)
+
+    words[1].english = "dolphin"
+    words[1].japanese = "イルカ"
+
+    words[2].english = "beluga"
+    words[2].japanese = "シロイルカ"
+
+    words[3].english = "grampus"
+    words[3].japanese = "シャチ"
+
+    words[4].english = "medusa"
+    words[4].japanese = "海月"
+
+    words[5].english = "otter"
+    words[5].japanese = "カワウソ"
+
+    # ハッシュテーブルの初期化
+    initHashTable(hash_table, 503);
+
+    # データを追加
+    for n in 1:5
+        addDataToMap(hash_table, words[n])
+    end
+
+    while n != 0
+
+        @printf "どの操作を行いますか?（1:検索 2:削除 3:全表示 0:終了）\n>"
+        scanf("%d", &n);
+        buf = paprse(Int, readline())
+        if buf == 1 # 検索
+            @printf "検索する英単語を入力してください："
+            key = readline()
+            japanese = getDataFromMap(hash_table, key)
+            if japanese != nothing
+                @printf "%sの和訳は%sです。\n" key japanese
+            else
+                @printf "%sがマップのなかに見つかりませんでした。\n" key
+            end
+        elseif buf == 2 # 削除
+            @printf "削除する英単語を入力してください："
+            key = readline()
+            wordfound = deleteDataFromMap(hash_table, key)
+            if wordfound != nothing
+                @printf "%sをマップから削除しました。\n" key
+            else
+                @printf "%sがマップのなかに見つかりませんでした。\n" key
+            end
+        elseif buf == 3 # 全表示
+            printAllData(hash_table)
+        end
+    end
+    #cleanupHashTable(hash_table) # クリーンアップ
+
+    return 0
 
 end
 
