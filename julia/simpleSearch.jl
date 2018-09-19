60328ea2abc5e0097bccaf73f8e53e951c406899
szmlb <szmlb.robotics@gmail.com>
2018-09-19 23:57:44 +0900

    Modify simpleSearch.jl

--- julia/simpleSearch.jl
+++ julia/simpleSearch.jl
@@ -1,30 +1,28 @@
 using Printf
 
-function simpleSearch(text, pattern, original_text)
+function simpleSearch(text, pattern)
 
-    while text != '\0'
+    for i in 1:length(text)-length(pattern)+1
         # わかりやすいように，いま何を比較しているか表示
-        @printf "    本文:%s \nパターン:" original_text
-        for i = 1:text-original_text
-            @printf "　"
+        @printf "    本文:%s \nパターン:" text
+        for j = 1:i-1
+            @printf " "
         end
         @printf "%s \n" pattern
 
         # パターンの先頭から比較を始める
-        i = 0
-        while pattern[i] != '\0'
+        for j in 1:length(pattern)
 
-            if pattern[i] != text[i]
-                break;              # 一致しなかった
+            if pattern[i+j-1] != text[j]
+                break              # 一致しなかった
             end
-            i = i + 1
+
         end
-        if pattern[i] == '\0'        # 一致した
+        if j == length(pattern)        # 一致した
             return text;
         end
 
         # 一致しなかったので，テキストを1つずらして再度挑戦
-        text = text + 1
     end
 
     return false
@@ -35,7 +33,7 @@ function main()
     original_text = "Team Swift"
     original_pattern = "if"
 
-    result = simpleSearch(original_text, original_pattern, original_text)
+    result = simpleSearch(original_text, original_pattern)
     if result == false
         @printf "見つかりませんでした\n"
     else
